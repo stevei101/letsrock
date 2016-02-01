22462f1bbeb1b4eba119843b052fe80508051a14
stevei101 <steven.irvin@gmail.com>
2016-02-01 12:33:24 -0800

    fix path

--- elk.sh
+++ elk.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-if [ -d "$ansible-playbook-elk/" ]; then
+if [ -d "ansible-playbook-elk" ]; then
     # Control will enter here if $DIRECTORY exists.
-    rm -Rf ansible-playbook-elk/
+    sudo rm -Rf ansible-playbook-elk
 fi
 git clone https://github.com/kosssi/ansible-playbook-elk.git
 

0f959e1416914f00610e8e9133eef5d2f2b614c9
stevei101 <steven.irvin@gmail.com>
2016-02-01 12:11:05 -0800

    update basic

--- basic.sh
+++ basic.sh
@@ -1,7 +1,14 @@
-# for centos 
-sudo yum update; sudo yum install -y git gcc libffi-devel python-devel openssl-devel
+#!/bin/bash
+if [ -f /etc/redhat-release ]; then
+    # for centos 
+    sudo yum update;
+    sudo yum install -y git gcc libffi-devel python-devel openssl-devel
+fi
 
-# for ubuntu
-sudo apt-get update; sudo apt-get -y upgrade; sudo apt-get install -y build-essential libssl-dev libffi-dev python-devi
+if [ -f /etc/lsb-release ]; then
+    # for ubuntu
+    sudo apt-get update; sudo apt-get -y upgrade;
+    sudo apt-get install -y build-essential libssl-dev libffi-dev python-devi
+fi
 
 sudo pip install cryptography ansible

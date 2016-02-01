8554206c0e76f2567f805f93c278bf526790c55c
stevei101 <steven.irvin@gmail.com>
2016-02-01 12:39:59 -0800

    fix ansible

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 ansible-galaxy install -r ansible-playbook-elk/requirements.yml
-ansible-playbook -i "localhost," -C steven_irvin -c local ansible-playbook-elk/playbook.yml -vvvv
+ansible-playbook -i "localhost," -u steven_irvin -c local ansible-playbook-elk/playbook.yml -vvvv

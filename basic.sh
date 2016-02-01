14e7337276367efca4ad27b5241123dbc21c560e
stevei101 <steven.irvin@gmail.com>
2016-02-01 11:29:03 -0800

    + basic

---
+++ basic.sh
@@ -0,0 +1,7 @@
+# for centos 
+sudo yum update; sudo yum install -y git gcc libffi-devel python-devel openssl-devel
+
+# for ubuntu
+sudo apt-get update; sudo apt-get -y upgrade; sudo apt-get install -y build-essential libssl-dev libffi-dev python-devi
+
+sudo pip install cryptography ansible

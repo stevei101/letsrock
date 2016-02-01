349a4c313bb60941cf79d3514170f7269e5fef7a
stevei101 <steven.irvin@gmail.com>
2016-02-01 12:28:02 -0800

    check path

--- elk.sh
+++ elk.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/bash
+export DIRECTORY="ansible-playbook-elk/"
+if [ -d "$DIRECTORY" ]; then
+    # Control will enter here if $DIRECTORY exists.
+    rm -Rf ansible-playbook-elk/
+fi
 git clone https://github.com/kosssi/ansible-playbook-elk.git
-cd ansible-playbook-elk/
+cd "$DIRECTORY"

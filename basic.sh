7bcc96a5409167007d3e2f27bbf554376c275e01
stevei101 <steven.irvin@gmail.com>
2016-02-02 07:54:52 -0800

    + wget, install pip

--- basic.sh
+++ basic.sh
@@ -2,13 +2,15 @@
 if [ -f /etc/redhat-release ]; then
     # for centos 
     sudo yum update;
-    sudo yum install -y git gcc libffi-devel python-devel openssl-devel
+    sudo yum install -y wget git gcc libffi-devel python-devel openssl-devel
 fi
 
 if [ -f /etc/lsb-release ]; then
     # for ubuntu
     sudo apt-get update; sudo apt-get -y upgrade;
-    sudo apt-get install -y build-essential libssl-dev libffi-dev python-devi
+    sudo apt-get install -y wget build-essential libssl-dev libffi-dev python-devi
 fi
 
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
 sudo pip install cryptography ansible

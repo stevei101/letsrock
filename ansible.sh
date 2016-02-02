b80c2a68ed6f68b718a651a3f4ae822c4a441316
stevei101 <steven.irvin@gmail.com>
2016-02-02 09:03:40 -0800

    why the face?

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
+# ansible-galaxy install -r requirements.yml -p ./roles --ignore-errors
 ansible-galaxy install -r requirements.yml
 ansible-playbook -i "localhost," -c local playbook.yml -vvvv

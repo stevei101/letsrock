050485430f2a17994b0fb33ca113b8336532f69e
Steven Irvin <steven.irvin@gmail.com>
2016-02-06 08:37:57 -0800

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -2,6 +2,6 @@
 sudo yum update; sudo yum install -y git gcc libffi-devel python-devel openssl-devel
 
 # for ubuntu
-sudo apt-get update; sudo apt-get -y upgrade; sudo apt-get install -y build-essential libssl-dev libffi-dev python-devi
+sudo apt-get update; sudo apt-get -y upgrade; sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
 
 sudo pip install cryptography ansible
